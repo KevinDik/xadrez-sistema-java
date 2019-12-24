30f16c79b9398f1fb971c383a7ce5fa55994483f
KevinDik <kevindiegodasilvasousa@gmail.com>
2019-12-23 22:19:35 -0300

    Colocando algumas peças nos lugares

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -35,4 +35,9 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getRow()];
 	}//sobrecarga do metodo passando posicao
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}//pega matriz na posicao dada e atribui a peca informada no metodo
 }
