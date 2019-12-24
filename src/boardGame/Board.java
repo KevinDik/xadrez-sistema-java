84aa51aba254da57e677b7498daadc3f01363e94
KevinDik <kevindiegodasilvasousa@gmail.com>
2019-12-23 22:47:19 -0300

    Exceções e programação defensiva

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro criando tabuleiro, é necessário que tenha uma linha e uma coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,28 +19,42 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posição não está no tabuleiro");
+		}
 		return pieces[row][column];
 	}// retorna peca dada linha e coluna
 	
 	public Piece piece(Position position) {
-		return pieces[position.getRow()][position.getRow()];
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não está no tabuleiro");
+		}
+		return pieces[position.getRow()][position.getColumn()];
 	}//sobrecarga do metodo passando posicao
 	
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("A posição selecionada já existe uma peça" + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}//pega matriz na posicao dada e atribui a peca informada no metodo
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não está no tabuleiro");
+		}
+		return piece(position) != null;
+	}
 }
